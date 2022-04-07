cabf8b811bf0bf707e104c695f5a9622d4186ef2
swastik2703 <swastikvasistha2127@gmail.com>
2022-04-07 18:50:03 +0530

    vowel consonant program

---
+++ basic questions/vowel_const.py
@@ -0,0 +1,8 @@
+print("vowel or constatnt program")
+
+print("enter a character:")
+ch = input()
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U':
+    print("it is a vowel")
+else:
+    print("Its a consonant")
