a888d0d6bf3c1d51e955a9350c4e8b31ca1372a7
swastik2703 <swastikvasistha2127@gmail.com>
2022-04-20 23:34:05 +0530

    duplicates list

---
+++ list/duplicatelist.py
@@ -0,0 +1,25 @@
+#duplicates are allowed in list
+from math import trunc
+from operator import truediv
+
+
+items = ["apple","mango","banana","apple","cherry"]
+print(items)
+print(len(items))
+
+#datatypes  of list items
+l1 = [1,2,3,4]
+l2 = ["apple","mango","banana"]
+l3 = [True,False,False,True]
+print(l1,l2,l3)
+l4 = [1,2,"string",True]
+print(l4)
+
+#to identify the type of list
+list = [1,2,3,4]
+print(type(list))
+list1 = ["sv","fsug","tsugdc"]
+print(type(list1))
+list2 = [True,False,True]
+print(type(list2))
+
